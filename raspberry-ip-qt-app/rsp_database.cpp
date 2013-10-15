79214dc21135efd7f5db6d021341bf83388a1101
sandesh ghimire <sandeshghimire@outlook.com>
2013-10-15 13:19:21 -0700

    openDB, deleteDB and lastError support

--- raspberry-ip-qt-app/rsp_database.cpp
+++ raspberry-ip-qt-app/rsp_database.cpp
@@ -1,5 +1,18 @@
 #include "rsp_database.h"
 
-rsp_database::rsp_database()
+bool RspDatabase::openDB()
 {
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("my.db.sqlite");
+    return db.open();
+}
+bool RspDatabase::deleteDB()
+{
+    return QFile::remove("my.db.sqlite");
+}
+
+QSqlError RspDatabase::lastError()
+{
+    return db.lastError();
+
 }
--- raspberry-ip-qt-app/rsp_database.h
+++ raspberry-ip-qt-app/rsp_database.h
@@ -1,10 +1,28 @@
 #ifndef RSP_DATABASE_H
 #define RSP_DATABASE_H
 
-class rsp_database
+#include <QObject>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QObject>
+#include <QFile>
+
+
+class RspDatabase : public QObject
 {
+
 public:
-    rsp_database();
+    RspDatabase(QObject *parent = 0);
+    ~RspDatabase();
+public:
+    bool openDB();
+    bool deleteDB();
+    QSqlError lastError();
+
+
+private:
+    QSqlDatabase db;
+
 };
 
 #endif // RSP_DATABASE_H
